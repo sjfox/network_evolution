41c823435c71184ab9ba080aa9b3e087e97e38de
Rachel Gicquelais <rgic@umich.edu>
2017-06-23 16:34:30 -0600

    Code Comments

--- R/testing_graph_build.R
+++ R/testing_graph_build.R
@@ -13,6 +13,8 @@ edges_to_attach <- 2
 alpha <- 2
 init_graph_length <- length(V(g))
 graph_list <- vector(mode = "list", length = (tot_nodes - init_graph_length))
+
+# At each step, save the graph  in a list
 for(node_index in (init_graph_length+1):tot_nodes){
   ## Cycles through desired number of nodes, starting with index of current graph + 1
   ## This index should always start at 4, because graph should initialize with 3, but
@@ -28,15 +30,28 @@ for(node_index in (init_graph_length+1):tot_nodes){
   graph_list[[node_index-init_graph_length]] <- g
 }
 
+# Modularity
+
+# Function to summarize modularity for each graph (called below over all graphs)
+# Cluster_louvain clusters the graph into communities 
+# Modularity is a metric for how the graph is clustered into communities and how connected the communities are
+# Modularity decreases as the connectections between communities increase
 
 get_modularity <- function(graph){
   ## Returns the modularity for a single graph
   modularity(x = graph, membership = membership(cluster_louvain(graph)))
 }
 
-
-
+# Cycles thru each item (graph at each point in time), 
+# Give each item in list to function get_modularity,
+# Unlist makes into a vector to plot
 graph_list %>% purrr::map(~get_modularity(.x)) %>% unlist() -> test
+
+# Transitivity
+
+# Here, transitivity is a graph-level measure of the clustering coefficient
+# Clustering coefficient reflects # triangles in graph
+# For loop above has calculation for node-level clustering coefficient
 
 graph_list %>% purrr::map(~transitivity(.x)) %>% unlist() -> test
 
